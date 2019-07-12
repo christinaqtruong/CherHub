1aa17c8308f8e6dddc5d90c30166c7443146e6e9
Christina Truong <christinaqtruong@gmail.com>
2019-07-11 20:27:55 -0500

    Got modal to pop up with image

--- app/data/cherubs.js
+++ app/data/cherubs.js
@@ -1,29 +1,17 @@
-// var path = require("path");
+var cherubs = [
+    {
+        "name": "Henry Cavill",
+        "picture": "https://www.flare.com/wp-content/uploads/2018/07/Henry-Cavill-close-up.jpeg",
+        "scores": ["4", "2", "1", "5", "3", "2", "1", "5", "3", "3"]
+    },{
+        "name": "Michael Fassbender",
+        "picture": "https://i.mdel.net/i/db/2017/7/732182/732182-800w.jpg",
+        "scores": ["2", "2", "3", "3", "5", "1", "1", "4", "3", "5"]
+    },{
+        "name": "Brad Pitt",
+        "picture": "https://www.etonline.com/sites/default/files/styles/max_640x640/public/images/2019-05/gettyimages-1145337999.jpg?itok=oUdrKdUK&h=6b7011a3",
+        "scores": ["2", "4", "3", "4", "5", "5", "2", "2", "3", "1"]
+    }
+];
 
-// module.exports = function(app) {
-// // Submit user data
-// $("#submit-btn").on("click", function(event) {
-//     event.preventDefault();
-
-//     var newCherub = {
-//       "name": $("#username").val().trim(),
-//       "picture": $("#profile-picture").val().trim(),
-//       "scores": [
-//           //grab user score using if statement? or value? how to check if the user selected that value and then grab it
-//       ]
-//     }
-
-//       for(var i=0; i < newCherub.scores.length; i++){
-//         var scores = [];
-//         scores.push(newCherub.scores[i])
-//         var difference = function (a, b) { return Math.abs(a - b); }
-//         difference(scores[i],)
-//       };
-
-    
-//     $.post("/api/characters", newCherub)
-//       .then(function(data) {
-//         console.log("add.html", data);
-//       });
-//   });
-// }
+module.exports = cherubs;
